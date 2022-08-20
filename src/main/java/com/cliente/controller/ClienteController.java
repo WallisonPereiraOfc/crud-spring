53f56f51e4d54b89478efb6e5ddd2a3bd4fbd546
wallison Pereira <110045968+WallisonPereiraOfc@users.noreply.github.com>
2022-08-19 22:43:45 -0300

    Add files via upload

---
+++ src/main/java/com/cliente/controller/ClienteController.java
@@ -0,0 +1,13 @@
+package com.cliente.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ClienteController {
+
+    @RequestMapping("/")
+    public String Index(){
+        return "index";
+    }
+}
